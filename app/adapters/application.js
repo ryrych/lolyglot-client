3d89ee787c167a7ef1b6171c677aa785f11e3f50
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-08-25 16:56:59 +0200

    Protected lol resource

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,7 @@
-import DS from 'ember-data';
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
   namespace:  'api/v1',
   host: 'http://localhost:3000',
   authorizer: 'authorizer:doorkeeper'
